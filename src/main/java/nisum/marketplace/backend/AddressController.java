136dfb5d07e224e16bf74d8e29c8832cf5d20c13
ViP-Cente <vpericone@mail.sfsu.edu>
2022-09-20 16:37:23 -0700

    Impelmented API for addresses along with exceptions

---
+++ src/main/java/nisum/marketplace/backend/AddressController.java
@@ -0,0 +1,73 @@
+package nisum.marketplace.backend;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import nisum.marketplace.backend.model.Address;
+import nisum.marketplace.backend.service.UserAddressService;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/address")
+public class AddressController {
+
+	@Autowired
+	UserAddressService service;
+	
+	@GetMapping("/getAllAddresses")
+	public ResponseEntity<List<Address>> getAllAddresses(){
+		return new ResponseEntity<List<Address>>((service.getAll()), HttpStatus.OK);
+	}
+	
+	@GetMapping("/getAddress/{id}")
+	public ResponseEntity<?> getAddressById(@PathVariable Integer id){
+		try {
+			return new ResponseEntity<Address>(service.getAddressById(id), HttpStatus.OK);
+		}catch (Exception e){
+			return new ResponseEntity<String>("Item " + id + " not found", HttpStatus.CONFLICT);
+		}
+	}
+	
+	@DeleteMapping("/deleteAddress/{id}")
+	public ResponseEntity<?> deleteAddress(@PathVariable Integer id) {
+		try {
+			return new ResponseEntity<Boolean>(service.deleteAddressById(id), HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<String>("Item " + id + " not found", HttpStatus.CONFLICT);
+		}
+	}
+	
+	@PutMapping("/updateAddress/{id}")
+	public ResponseEntity<?> updateAddress(@PathVariable Integer id, @RequestBody Address address) {
+		try {
+			return new ResponseEntity<Address>(service.updateAddress(id, address), HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<String>("Item " + id + " not found", HttpStatus.CONFLICT);
+		}
+	}
+	
+	@PostMapping("/createAddress")
+	public ResponseEntity<?> createAddress(@RequestBody Address address) {
+		try {
+			return new ResponseEntity<Address>(service.createAddress(address), HttpStatus.CREATED);
+		}catch(Exception e) {
+			return new ResponseEntity<String>("Item " + address.getAddressID() + " not created", HttpStatus.CONFLICT);
+		}
+	}
+	
+	
+}
---
+++ src/main/java/nisum/marketplace/backend/model/Address.java
@@ -0,0 +1,126 @@
+package nisum.marketplace.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "Addresses")
+@Entity
+public class Address {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	Integer addressID;
+	Integer userID;
+	String recipientName;
+	String street;
+	String street2;
+	String city;
+	String state;
+	String zip;
+	Boolean isShipping;
+	Boolean isBilling;
+	
+	public Address() {
+		
+	}
+
+	public Address(Integer addressID, Integer userID, String recipientName, String street, String street2,
+			String city, String state, String zip, Boolean isShipping, Boolean isBilling) {
+		super();
+		this.addressID = addressID;
+		this.userID = userID;
+		this.recipientName = recipientName;
+		this.street = street;
+		this.street2 = street2;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.isShipping = isShipping;
+		this.isBilling = isBilling;
+	}
+
+	public Integer getAddressID() {
+		return addressID;
+	}
+
+	public void setAddressID(Integer addressID) {
+		this.addressID = addressID;
+	}
+
+	public Integer getUserID() {
+		return userID;
+	}
+
+	public void setUserID(Integer userID) {
+		this.userID = userID;
+	}
+
+	public String getRecipientName() {
+		return recipientName;
+	}
+
+	public void setRecipientName(String recipientName) {
+		this.recipientName = recipientName;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getStreet2() {
+		return street2;
+	}
+
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public Boolean getIsShipping() {
+		return isShipping;
+	}
+
+	public void setIsShipping(Boolean isShipping) {
+		this.isShipping = isShipping;
+	}
+
+	public Boolean getIsBilling() {
+		return isBilling;
+	}
+
+	public void setIsBilling(Boolean isBilling) {
+		this.isBilling = isBilling;
+	}
+	
+	
+	
+}
