037978ec48005b6462f0830f66c50561ad4ad520
ViP-Cente <vpericone@mail.sfsu.edu>
2022-09-21 16:39:56 -0700

    Fixed bug to let the ID autoincrement

--- src/main/java/nisum/marketplace/backend/service/CreditCardImpl.java
+++ src/main/java/nisum/marketplace/backend/service/CreditCardImpl.java
@@ -8,58 +8,54 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class CreditCardImpl implements CreditCardService{
-    @Autowired
-    CreditCardRepository cardRepository;
+public class CreditCardImpl implements CreditCardService {
+	@Autowired
+	CreditCardRepository cardRepository;
 
+	@Override
+	public List<CreditCard> getAll() {
+		return cardRepository.findAll();
+	}
 
-    @Override
-    public List<CreditCard> getAll(){
-        return cardRepository.findAll();
-    }
+	@Override
+	public CreditCard getCreditCardByID(Integer creditCardID) throws Exception {
+		if (cardRepository.findById(creditCardID).isEmpty()) {
+			System.out.println("Card with id:" + creditCardID + " could not be found.");
+			throw new Exception();
+		} else {
+			return cardRepository.findById(creditCardID).get();
+		}
+	}
 
-    @Override
-    public CreditCard getCreditCardByID(Integer creditCardID) throws Exception{
-        if(cardRepository.findById(creditCardID).isEmpty()){
-            System.out.println("Card with id:" + creditCardID +" could not be found.");
-            throw new Exception();
-        }
-        else{
-            return cardRepository.findById(creditCardID).get();
-        }
-    }
+	@Override
+	public CreditCard createCreditCard(CreditCard creditCard) throws Exception {
+		if (creditCard.getCreditCardID() != null) {
+			if (cardRepository.existsById(creditCard.getCreditCardID())) {
+				System.out.println("Card could not be created. Card already exists.");
+				throw new Exception();
+			}
+		}
+		return cardRepository.save(creditCard);
+	}
 
-    @Override
-    public CreditCard createCreditCard(CreditCard creditCard) throws Exception {
-        if(cardRepository.existsById(creditCard.getCreditCardID())){
-            System.out.println("Card could not be created. Card already exists.");
-            throw new Exception();
-        }
-        else{
-            return cardRepository.save(creditCard);
-        }
-    }
+	@Override
+	public CreditCard updateCreditCard(Integer creditCardID, CreditCard creditCard) throws Exception {
+		if (!cardRepository.existsById(creditCard.getCreditCardID())) {
+			System.out.println("Card could not be updated. Card does not exists.");
+			throw new Exception();
+		} else {
+			return cardRepository.save(creditCard);
+		}
+	}
 
-    @Override
-    public CreditCard updateCreditCard(Integer creditCardID, CreditCard creditCard) throws Exception{
-        if(!cardRepository.existsById(creditCard.getCreditCardID())){
-            System.out.println("Card could not be updated. Card does not exists.");
-            throw new Exception();
-        }
-        else{
-            return cardRepository.save(creditCard);
-        }
-    }
-
-    @Override
-    public boolean deleteCreditCardByID(Integer creditCardID) throws Exception{
-        if(!cardRepository.existsById(creditCardID)){
-            System.out.println("Card could not be deleted. Card does not exists.");
-            throw new Exception();
-        }
-        else{
-            cardRepository.deleteById(creditCardID);
-            return true;
-        }
-    }
+	@Override
+	public boolean deleteCreditCardByID(Integer creditCardID) throws Exception {
+		if (!cardRepository.existsById(creditCardID)) {
+			System.out.println("Card could not be deleted. Card does not exists.");
+			throw new Exception();
+		} else {
+			cardRepository.deleteById(creditCardID);
+			return true;
+		}
+	}
 }
