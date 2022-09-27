3dd0874777b2f46f7869e3429aad8a47f2aeea92
carlos <clemus@nisum.com>
2022-09-26 17:30:36 -0700

    updating

--- src/main/java/nisum/marketplace/backend/BackendApplication.java
+++ src/main/java/nisum/marketplace/backend/BackendApplication.java
@@ -14,12 +14,13 @@ import nisum.marketplace.backend.service.UserAddressService;
 @SpringBootApplication(scanBasePackages = "nisum.*")
 @EnableJpaRepositories(basePackages = "nisum.*")
 @EntityScan(basePackages = "nisum.*")
-public class BackendApplication implements CommandLineRunner{
+public class BackendApplication{ // implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	/*
 	@Autowired
 	OrdersService service2;
 	
@@ -28,6 +29,6 @@ public class BackendApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println(service2.getOrders());
 	}
-
+	*/
 
 }
